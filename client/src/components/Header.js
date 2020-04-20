9d37a6cbab9dc5637879f2d22f1b5fae170f8926
Adeyinka Giwa <adeyinka36@gmail.com>
2020-04-20 15:44:58 +0100

    made courses logo into a link to the home page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink} from 'react-router-dom';
+import { Link,NavLink} from 'react-router-dom';
 
 
 
@@ -11,7 +11,7 @@ if(auth){
     return(
             <div className="header">
             <div className="bounds">
-                <h1 className="header---logo">Courses</h1>
+                <Link to="/"><h1 className="header---logo">Courses</h1></Link>
                 <nav><span>{`Welcome ${auth.firstName} ${auth.lastName}`}</span><NavLink className="signout" to="/signout-UserSignOut">Sign Out</NavLink></nav>:
                   
             </div>
@@ -22,7 +22,7 @@ if(auth){
     return(
         <div className="header">
         <div className="bounds">
-            <h1 className="header---logo">Courses</h1>
+        <Link to="/"><h1 className="header---logo">Courses</h1></Link>
               <nav><NavLink className="signup" to="/signup-UserSignUp">Sign Up</NavLink></nav>
              <nav> <NavLink  className="signin" to="/signin-UserSignIn">Sign In</NavLink></nav>
         </div>
